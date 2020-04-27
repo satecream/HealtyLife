53ca1dffc91a5327e24e9a56658a003be1c61bcd
satecream <51484253+satecream@users.noreply.github.com>
2020-04-27 20:58:42 +0700

    27 april 2020 05:18 , pembaruan untuk menu dan image slider

--- app/src/main/java/com/hidupsehat/healtylife/MainActivity.java
+++ app/src/main/java/com/hidupsehat/healtylife/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hidupsehat.healtylife;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,20 +12,52 @@ import android.widget.ViewFlipper;
 public class MainActivity extends AppCompatActivity {
 
     ViewFlipper v_flipper;
+    private CardView notif, setting, video, info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* -------------------------------notification------------------------------------*/
+        notif = findViewById(R.id.cvnotif);
+        notif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Notification.class);
+                startActivity(i);
+            }});
+        /* -----------------------------end notification--------------------------------------*/
+        /* -------------------------------setting------------------------------------*/
+        setting = findViewById(R.id.cvsetting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Setting.class);
+                startActivity(i);
+            }});
+        /* -----------------------------end setting--------------------------------------*/
+        /* -------------------------------video------------------------------------*/
+        video = findViewById(R.id.cvvideo);
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Video.class);
+                startActivity(i);
+            }});
+        /* -----------------------------end video--------------------------------------*/
+        /* -------------------------------information------------------------------------*/
+        info = findViewById(R.id.cvinfo);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Information.class);
+                startActivity(i);
+            }});
+        /* -----------------------------end information--------------------------------------*/
+        /* -----------------------------view flipper--------------------------------------*/
         int images[] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
-
         v_flipper = findViewById(R.id.v_flipper);
-/*
-        for (int i = 0; i < images.length; i++){
-            flipperImages(images[i]);
-        }
-
- */
         for (int image : images) {
             flipperImages(image);
         }
@@ -31,14 +65,14 @@ public class MainActivity extends AppCompatActivity {
     void flipperImages(int image){
         ImageView imageView = new ImageView(this);
         imageView.setBackgroundResource(image);
-
         v_flipper.addView(imageView);
         v_flipper.setFlipInterval(4000);
         v_flipper.setAutoStart(true);
         v_flipper.setInAnimation(this, R.anim.slide_in_left);
         v_flipper.setInAnimation(this, R.anim.slide_out_right);
-
     }
+        /* -----------------------------end flipper--------------------------------------*/
+
 
     public void sendMessage(View view) {
     }
